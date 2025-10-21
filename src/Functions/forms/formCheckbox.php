89ddc55f60175afc1b5c0f4b5a94f3a68e4d57d8
krzysztofzylka <krzysztofzylka@yahoo.com>
2025-10-21 18:46:43 +0200

    #1280 Dodanie checkboxa do funkcji formularzy

---
+++ src/Functions/forms/formCheckbox.php
@@ -0,0 +1,42 @@
+<?php
+
+use NimblePHP\Form\Enum\MethodEnum;
+use Twig\Markup;
+
+/**
+ * @param string $name
+ * @param array $options
+ * @param string|array|null $selectedKey
+ * @param string|null $title
+ * @param array $attributes
+ * @return false|Markup
+ */
+function formCheckbox(string $name, ?string $title = null, array $attributes = []): false|Markup
+{
+    $form = new class () {
+        use \NimblePHP\Form\Traits\Field;
+        public \NimblePHP\Form\Enum\MethodEnum $method = MethodEnum::POST;
+        public \NimblePHP\Framework\Request $request;
+        public array $validationErrors = [];
+        
+        public function __construct()
+        {
+            $this->validationErrors = \NimblePHP\Form\Form::$VALIDATIONS;
+        }
+
+        public function getData(): array
+        {
+            return $_POST;
+        }
+
+        public function __toString(): string
+        {
+            return $this->renderField($this->fields[0]);
+        }
+    };
+
+    $form->request = new \NimblePHP\Framework\Request();
+    $form->addCheckbox($name, $title, $attributes);
+
+    return new Markup($form, 'UTF-8');
+}
