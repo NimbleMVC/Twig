7fee1d967859997ce78d6bdc695a8ee663ead425
Krzysztof Żyłka <krzysztofzylka@yahoo.com>
2026-02-17 19:06:24 +0100

    #1602 Poprawa serwisów

--- src/Module.php
+++ src/Module.php
@@ -3,21 +3,31 @@
 namespace NimblePHP\Twig;
 
 use NimblePHP\Framework\Config;
-use NimblePHP\Framework\Interfaces\CliCommandProviderInterface;
+use NimblePHP\Framework\DataStore;
+use NimblePHP\Framework\Exception\NimbleException;
 use NimblePHP\Framework\Kernel;
 use NimblePHP\Framework\Module\Interfaces\ModuleInterface;
-use NimblePHP\Migrations\Commands\MigrationCommand;
+use Twig\Loader\FilesystemLoader;
 
 class Module implements ModuleInterface
 {
 
+    /**
+     * @return string
+     */
     public function getName(): string
     {
         return 'Nimblephp Twig views';
     }
 
+    /**
+     * @return void
+     * @throws NimbleException
+     */
     public function register(): void
     {
+        Kernel::$serviceContainer->set('twig.filesystemloader', new FilesystemLoader());
+
         if (Config::get('TWIG_ADD_SERVICE', true)) {
             $twig = new Twig();
 
